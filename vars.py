c52f7b16bcbb3f890c57b1623a6e85b337d46244
rajcricket <cricket.fever.forever45@gmail.com>
2026-02-17 10:32:37 +0530

    Update OWNER and AUTH_USERS in vars.py

--- vars.py
+++ vars.py
@@ -6,12 +6,13 @@ from os import environ
 API_ID = int(environ.get("API_ID", "xxxxxxxxx"))
 API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxxxxx")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "5680454765"))
+OWNER = int(environ.get("OWNER", "6400558510"))
 CREDIT = environ.get("CREDIT", "")
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '6400558510').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
